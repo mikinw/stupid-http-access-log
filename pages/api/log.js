e437d997b68f9f8c5ac992b5b7f1ee224bb3c7e3
Miklos Jakab <jakab.miklos@gmail.com>
2025-01-24 12:58:33 +0100

    Vercel api

--- pages/api/log.js
+++ pages/api/log.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         contentType: 'text/plain',
       });
 
-      const msg = `Log updated ${new Date().toISOString()}``;
+      const msg = `Log updated ${new Date().toISOString()}`;
 
       return res.status(200).json({ success: true, message: msg });
     } catch (err) {
