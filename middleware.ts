5206e204e32bb0118cb225305d3a9771b54dad2f
Miklos Jakab <jakab.miklos@gmail.com>
2025-01-24 11:00:04 +0100

    Vercel.

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: '/',
+  matcher: '/*',
 };
 
 export default function middleware(request: NextRequest) {
     console.log(`Visitor from ${request.nextUrl.pathname}`);
-
+    request.nextUrl.pathname = '/';
+    return NextResponse.rewrite(request.nextUrl);
 }
