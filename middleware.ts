87028aafd72d2386264746b80757b178be1c5cd1
Miklos Jakab <jakab.miklos@gmail.com>
2025-01-24 13:03:26 +0100

    Vercel api

--- middleware.ts
+++ middleware.ts
@@ -16,11 +16,20 @@ export default function middleware(request: Request) {
               headers: { 'content-type': 'application/json' },
           },
         );
-    }
-    if (url.pathname.startsWith('/list')) {
-        return Response.
-
     } */
+    if (url.pathname.startsWith('/list')) {
+        try {
+
+          const existingBlob = await fetch(LOG_FILE_KEY);
+    
+          const logContent = await existingBlob.text(); // Get the log file content
+          return res.status(200).end(logContent);
+        } catch (err) {
+          console.error('Error reading log file:', err);
+          return res.status(500).json({ error: 'Failed to read log file' });
+        }
+
+    }
     //return NextResponse.rewrite(request.nextUrl);
     return NextResponse.next();
 }
